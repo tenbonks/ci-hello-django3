50b1a6c13ec776e3af4870d2c3825769f068dbc7
Ben Tonks <thebentonks@gmail.com>
2020-05-27 16:34:59 +0100

    imported get_todo_list and set it as base url

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import say_hello
+from todo.views import get_todo_list
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name="hello")
+    path('', get_todo_list, name="get_todo_list")
 ]
