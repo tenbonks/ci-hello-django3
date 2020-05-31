8b445bfeb7efa8c98a2e7c7c92226e6a06d4cb67
Ben Tonks <thebentonks@gmail.com>
2020-05-31 17:03:51 +0100

    using a remote DB, imported dj_database_url

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://voycqfkxzcsnlk:34404f0813c302ec904820020e3f1aa4285b69890d1d3a28df32982b013fbab7@ec2-79-125-26-232.eu-west-1.compute.amazonaws.com:5432/dcmakgptmlmjlp')
 }
 
 
