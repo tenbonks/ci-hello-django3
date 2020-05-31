ca661c0f7a769795e52eecce13661eb3a5006ae2
Ben Tonks <thebentonks@gmail.com>
2020-05-31 13:24:13 +0100

    created test_can_edit_item

--- todo/test_views.py
+++ todo/test_views.py
@@ -19,7 +19,7 @@ class testViews(TestCase):
         self.assertTemplateUsed(response, 'todo/add_item.html')
 
 
-    def test_edit_item_page(self):
+    def test_get_edit_item_page(self):
         item = Item.objects.create(name='Test Todo Item')
         response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
@@ -44,3 +44,11 @@ class testViews(TestCase):
         self.assertRedirects(response, '/')
         updates_item = Item.objects.get(id=item.id)
         self.assertFalse(updates_item.done)
+
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name' : 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
